aa79d41bf02b9d5fc7c4c8c4bf12fadc39cdadbd
Wanderson Lima <wanderson.devops@gmail.com>
2023-07-22 09:56:01 -0400

    Consuming API

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,29 +1,61 @@
 import React from 'react';
 import {FiChevronRight} from 'react-icons/fi'
+import {api} from '../../services/api'
+
 import {Title, Form, Repos} from './styles'
 
 import logo from '../../assets/logo.svg'
 
+interface GithubRepository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  }
+}
 export const Dashboard: React.FC = () => {
-    return (
+
+  const [repos, setRepos] = React.useState<GithubRepository[]>([]);  
+  const [newRepo, setNewRepo] = React.useState('repos/${newRepo}');
+  
+  function handleInputChange(event: React.ChangeEvent<HTMLInputElement>): void {
+    setNewRepo(event.target.value);
+  }
+  async function handleAddRepo(
+    event: React.FormEvent<HTMLFormElement>,
+    ): Promise<void> {
+    event.preventDefault();
+    const response = await api.get<GithubRepository>(`repos/${newRepo}`)
+    const repository = response.data;
+
+      setRepos([...repos, repository]);
+      setNewRepo('');
+  }
+  return (
       <>
         <img src={logo} alt="GitCollection"/>
 
         <Title>Catálogo de Repositórios do GitHub</Title>
-        <Form>
-          <input placeholder="username/repositorie" />
+        <Form onSubmit={handleAddRepo}>
+          <input placeholder="username/repositorie" onChange={handleInputChange}/>
           <button type='submit'>Buscar</button>
         </Form>
 
         <Repos>
-          <a href="/repositories">
-            <img src="https://avatars.githubusercontent.com/u/43724476?v=4" alt="Repositorio" />
+          {repos.map(repository => (
+            <a href="/repositories" key={repository.full_name}>
+            <img 
+              src={repository.owner.avatar_url} 
+              alt={repository.owner.login}
+            />
             <div>
-              <strong>wandersondevops/reactjs-nextjs</strong>
-              <p>React and Next Course</p>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
             </div>
             <FiChevronRight size={20}/>
           </a>
+          ))}
         </Repos>
       </>
     );
