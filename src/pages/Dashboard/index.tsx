a15039932bab78dadd6cc11d26d6e594cefec6fa
Wanderson Lima <wanderson.devops@gmail.com>
2023-07-22 18:01:10 -0400

    Refactoring - duplicates

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -73,8 +73,8 @@ export const Dashboard: React.FC = () => {
       {inputError && <Error>{inputError}</Error>}
 
       <Repos>
-        {repos.map(repository => (
-          <Link to={`/repositories/${repository.full_name}`} key={repository.full_name}>
+        {repos.map((repository, index) => (
+          <Link to={`/repositories/${repository.full_name + index}`} key={repository.full_name}>
             <img src={repository.owner.avatar_url} alt={repository.owner.login} />
             <div>
               <strong>{repository.full_name}</strong>
